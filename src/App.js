d141613dd3ff2fd4d6a33093e1bc4a7093205f95
thegreatsantini <lss13@my.fsu.edu>
2018-12-24 16:58:43 -0800

    adding all current user data to local storage

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
 
     this.state = {
       isAuthenticated: false,
-      userId: null
+      userId: null,
+      currentUser: null
     };
   }
 
@@ -26,20 +27,23 @@ class App extends Component {
 
   checkUser = () => {
     const currentUser = localStorage.getItem("userId");
+    const allData = localStorage.getItem('currentUser')
     if (currentUser) {
       this.setState({
         isAuthenticated: true,
-        userId: currentUser
+        userId: currentUser,
+        currentUser: JSON.parse(allData)
       });
     } else {
       console.log("no user from app");
     }
   };
 
-  authenticateUser = (isAuthenticated, userId) => {
+  authenticateUser = (isAuthenticated, userId, allData) => {
     this.setState({
       isAuthenticated,
-      userId
+      userId,
+      currentUser: allData
     });
   };
   render() {
@@ -47,7 +51,8 @@ class App extends Component {
       authenticateUser: this.authenticateUser,
       isAuthenticated: this.state.isAuthenticated,
       userId: this.state.userId,
-      showNotifier: this.showNotifier
+      showNotifier: this.showNotifier,
+      currentUser: this.state.currentUser
     };
     return (
       <React.Fragment>
