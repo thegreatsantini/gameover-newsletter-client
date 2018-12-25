16234e24b19baa347f29a5607f2966544cbff91b
thegreatsantini <lss13@my.fsu.edu>
2018-12-25 09:09:28 -0800

    added rotues to new components;

--- src/Routes.js
+++ src/Routes.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from './components/AppliedRoute';
-// import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
 import Home from './containers/Home';
 import Login from './containers/Login';
 import NotFound from './components/NotFound';
 import Signup from './containers/Signup';
-// import SandBox from './SandBox'
+import SandBox from './SandBox'
+import Users from './containers/Users'
+// import User from './containers/User'
+import Games from './containers/Games'
 import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 
 export default ({ childProps }) =>
@@ -14,7 +17,12 @@ export default ({ childProps }) =>
     {/* <UnauthenticatedRoute path="/" exact component={Landing} props={childProps} /> */}
     <UnauthenticatedRoute path="/signup" exact component={Signup} props={childProps} />
     <UnauthenticatedRoute path="/login" exact component={Login} props={childProps} />
+
+    {/* <AuthenticatedRoute path="/user" exact component={User} props={childProps} /> */}
+    <AuthenticatedRoute path="/users" exact component={Users} props={childProps} />
+    <AuthenticatedRoute path="/games" exact component={Games} props={childProps} />
+
     <AppliedRoute path="/" exact component={Home} props={childProps} />
-    {/* <AppliedRoute path='/sandbox' exact component={SandBox} /> */}
+    <AppliedRoute path='/sandbox' exact component={SandBox} />
     <Route component={NotFound} />
   </Switch>;
