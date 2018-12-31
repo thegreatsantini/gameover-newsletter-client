63cebf2a38f6ddc6e1a9f2e072c1c88bb812c844
thegreatsantini <lss13@my.fsu.edu>
2018-12-31 13:29:02 -0800

    User can remove card from watchlist

--- src/containers/User.js
+++ src/containers/User.js
@@ -1,21 +1,21 @@
 import React, { Component } from "react";
-// import axios from "axios";
+import Loading from "../components/Loading";
 import Button from "@material-ui/core/Button";
-import { currentUser, removeGame, removeFriend, visitFriend } from "../api";
+import UserTabs from "./UserTabs";
+import { currentUser, unWatch, removeFriend, visitFriend } from "../api";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
-  p: {
-    border: "solid red 2px"
-  },
   button: {
-    margin: theme.spacing.unit,
-  },
+    margin: theme.spacing.unit
+  }
 });
 
 class User extends Component {
   state = {
-    isLoading: true
+    isLoading: true,
+    value: 0,
+    watchlist: []
   };
 
   visitUser = async friendId => {
@@ -32,9 +32,24 @@ class User extends Component {
     console.log(result);
   };
 
-  removeGame = async (userId, watchlist) => {
-    const result = await removeGame(userId, watchlist);
-    console.log(result);
+  removeGame = async rowId => {
+    const { watchlist } = this.state;
+    const { userId } = this.props;
+    this.setState(
+      {
+        watchlist: watchlist.filter(game => {
+          if (game.rowId !== rowId) {
+            return game;
+          }
+        })
+      },
+      () => {
+        const rows = this.state.watchlist.map(game => game.rowId).join(",");
+        unWatch(userId, rows)
+          .then(res => console.log(res))
+          .catch(err => console.log(err));
+      }
+    );
   };
 
   showPopUp = () => {
@@ -49,62 +64,67 @@ class User extends Component {
         email,
         userName,
         followers,
-        watchlist,
-        isLoading: false
+        watchlist
       });
     } catch (err) {
       alert(`Error fetching user data: ${err.message}`);
-      this.setState({ isLoading: false })
     }
+    this.setState({ isLoading: false });
   }
 
+  handleChange = (event, value) => {
+    this.setState({ value });
+  };
+
   render() {
-    const { classes } = this.props;
+    const { classes, userId } = this.props;
+    const { isLoading, value, watchlist } = this.state;
     return (
       <React.Fragment>
-        <div className={classes.root}>
-          <h1>User homepage</h1>
-          <p>ID: {this.props.userId}</p>
-          <Button
-            onClick={this.getCurrentUser.bind(null, this.props.userId)}
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            Get User
-          </Button>
-          <Button
-            onClick={this.removeGame.bind(null, this.props.userId, ["games"])}
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            remove game
-          </Button>
-          <Button
-            onClick={this.unfollow.bind(
-              null,
-              this.props.userId,
-              "4156663718537092"
-            )}
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            remove friend
-          </Button>
-          <Button
-            onClick={this.visitUser.bind(
-              null,
-              "f7f0b3d0-073c-11e9-a2f0-e5c5ec8ad0ca"
-            )}
-            variant="contained"
-            color="primary"
-            className={classes.button}
-          >
-            visit friend
-          </Button>
-        </div>
+        {!isLoading ? (
+          <div className={classes.root}>
+            <UserTabs
+              removeGame={this.removeGame}
+              userId={userId}
+              games={watchlist}
+              value={value}
+              handleChange={this.handleChange}
+            />
+            <Button
+              onClick={this.getCurrentUser.bind(null, this.props.userId)}
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              Get User
+            </Button>
+            <Button
+              onClick={this.unfollow.bind(
+                null,
+                this.props.userId,
+                "4156663718537092"
+              )}
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              remove friend
+            </Button>
+            <Button
+              onClick={this.visitUser.bind(
+                null,
+                "f7f0b3d0-073c-11e9-a2f0-e5c5ec8ad0ca"
+              )}
+              variant="contained"
+              color="primary"
+              className={classes.button}
+            >
+              visit friend
+            </Button>
+          </div>
+        ) : (
+          <Loading fontSize={48} />
+        )}
       </React.Fragment>
     );
   }
